28c0f040e08d86d0a00b3640f9ee6c50e4a54e57
chenxiaowei <2332204966@qq.com>
2017-11-27 10:01:03 +0800

    加入工程

---
+++ NO1/NO1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NO1
+//
+//  Created by xs on 2017/11/27.
+//  Copyright © 2017年 Touker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
