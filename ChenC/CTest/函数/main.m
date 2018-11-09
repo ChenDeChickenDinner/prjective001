4ddeb3bb9d6eb792bc75e258c3c728bfec8c08f0
chenxiaowei <2332204966@qq.com>
2018-11-09 16:52:57 +0800

    编辑

---
+++ ChenC/CTest/函数/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  函数
+//
+//  Created by xs on 2018/11/9.
+//  Copyright © 2018 Touker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+void test0(){
+
+    return;
+}
+
+/**
+ 数组名作为形参
+1.实参的数组类型 必须与 形参的数组类型 相同
+2.形参的数组元素可以不写
+3.数组组外参数传递，传递的是首元素地址，形参的数组名与实参的数组名 公用一个地址，不在临时分配新地址
+4.
+ */
+void test1(){
+
+    return;
+}
---
+++ ChenC/CTest/特殊关键字以及作用域/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  特殊关键字以及作用域
+//
+//  Created by xs on 2018/11/9.
+//  Copyright © 2018 Touker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
