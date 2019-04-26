c0e5d150794d3c59cc3b4b6486711de10cff1eda
chenxiaowei <2332204966@qq.com>
2019-04-26 17:16:55 +0800

    xxxx

--- Grammar/OCGrammar/OCGrammar/main.m
+++ Grammar/OCGrammar/OCGrammar/main.m
@@ -10,8 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+#if !OBJC_TYPES_DEFINED
         printf("文件名:%s,行数:%d,函数名:%s\n",__FILE__,__LINE__,__func__);
 
+#endif
 
     }
     return 0;
---
+++ Grammar/OCGrammar/OCGrammar/基础/2对象/1对象本质/1objc/CObjectStruct.m
@@ -0,0 +1,13 @@
+//
+//  CObjectStruct.m
+//  OCGrammar
+//
+//  Created by xs on 2019/4/26.
+//  Copyright © 2019 xs. All rights reserved.
+//
+
+#import "CObjectStruct.h"
+
+@implementation CObjectStruct
+
+@end
