c6b6fb3e6a211dbfc96b61cc01ed0c6c8956d534
xs <xs@Mac-miniChenX.local>
2019-11-15 17:02:36 +0800

    xxx

---
+++ 未命名文件夹/2020COC/2020COC/Object/1OC的类/2继承关系/2inheritance.m
@@ -0,0 +1,28 @@
+//
+//  inheritance(继承).m
+//  2020COC
+//
+//  Created by xs on 2019/7/15.
+//  Copyright © 2019 xax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+1.子类拥有父类的全部属性、方法
+
+
+2.子类可以重写父类的方法实现
+
+3.父类指针可以指向子类对象
+
+
+
+
+
+
+
+
+@end
+
+
+
