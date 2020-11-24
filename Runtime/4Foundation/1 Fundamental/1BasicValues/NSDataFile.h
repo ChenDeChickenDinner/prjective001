f2a9c70db7ce30ef21af1e58704fe88b9c9c6bcd
xs <xs@Mac-miniChenX.local>
2020-11-24 17:00:13 +0800

    111

---
+++ Runtime/4Foundation/1 Fundamental/1BasicValues/NSDataFile.h
@@ -0,0 +1,18 @@
+//
+//  NSDataFile.h
+//  Runtime
+//
+//  Created by xs on 2020/11/23.
+//  Copyright © 2020 xax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDataFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
--- Runtime/main.m
+++ Runtime/main.m
@@ -10,8 +10,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-       
-        
+        NSNotification
+        NSObject *cc = nil;
+        [cc copy];
 //        NSInteger
 //        NSUInteger
 //        CGFloat
@@ -21,30 +22,40 @@ int main(int argc, const char * argv[]) {
 //        NSRect
 //        NSIndexPath NSIndexSet
         
-//        NSValue NSNumber
+//        NSValue NSNumber  NSDecimal NSDecimalNumber
         
-//        NSString NSMutableString
+//        NSString NSMutableString NSScanner条件判断利器  NSTextCheckingResult
 //        NSCharacterSet 字符串处理->字符集
+        
+        
+        
+//        NSCoder
         
 //        NSFormatter.h
 //        NSNumberFormatter.h
 //        NSDateFormatter.h
+//        NSByteCountFormatter
+//          NSEnergyFormatter
+//        NSLengthFormatter
+//        NSListFormatter
+//        NSMassFormatter
+//        NSRelativeDateTimeFormatter
         
-        
-//        NSDictionary NSMutableDictionary
-//        NSSet NSMutableSet
-//        NSArray NSMutableArray  NSPointerArray
+//        NSDictionary NSMutableDictionary  NSCache
+//        NSSet NSMutableSet  NSOrderedSet
+//        NSArray NSMutableArray  NSPointerArray NSPointerFunctions
 //        NSMapTable NSHashTable
         
 //        NSEnumerator 数组->枚举器
 //        NSSortDescriptor 数组->排序
     
-//        NSArchiver 归档
+//
 
+//        NSStream 流
         
-        
-//        NSDate
-//        NSCalendar 日历
+//        NSDate NSDateComponentsFormatter
+//        NSDateInterval NSDateIntervalFormatter
+//        NSCalendar 日历 NSCalendarDate
         
         
         
@@ -54,27 +65,26 @@ int main(int argc, const char * argv[]) {
 //        NSExpression.h
 //        NSException.h
         
-//        NSBundle
+//        NSBundle NSUUID
 //        NSFileManager
 //        NSFileHandle
 //        NSFileWrapper
 //        NSFileCoordinator
+//        NSPathUtilities 路径工具
         
-        
-//        NSUserDefaults.h
 //        NSTimeZone.h
 //        NSTimer.h
-//        NSRunLoop.h
 //        NSHost.h
 //        NSError.h
         
-        
-//        NSAutoreleasePool 内存管理
-//        NSThread 线程
+        //        NSRunLoop.h
+//        NSAutoreleasePool 内存管理 NSGarbageCollector垃圾回收
+//        NSThread 线程  NSConnection   NSProcessInfo进程 NSXPCConnection
 //        NSTask 操作
 //        NSOperation  任务
-//        NSLock 锁
+//        NSLock 锁 NSDistributedLock
         
+//        NSProgress 进度指示器
         
 //        NSURLSession.h
 //        NSURLResponse.h
@@ -98,6 +108,8 @@ int main(int argc, const char * argv[]) {
 //        NSHTTPCookieStorage.h
 //        NSHTTPCookie.h
         
+//        NSCoding  NSArchiver  NSKeyedArchiver  NSJSONSerialization NSPropertyListSerialization 归档序列化
+        
         
 //        NSXMLParser.h
 //        NSXMLNodeOptions.h
@@ -106,6 +118,23 @@ int main(int argc, const char * argv[]) {
 //        NSXMLDTDNode.h
 //        NSXMLDTD.h
 //        NSXMLDocument.h
+        
+//        NSPredicate NSComparisonPredicate NSCompoundPredicate  表示一个可以用来过滤对象集合的条件
+//        NSRegularExpression
+//
+//
+//        Type Encodings NSMethodSignature  NSInvocation 消息转发  NSUndoManager
+//        NSProxy NSDistantObject  NSProtocolChecker  消息转发
+//
+//
+//        NSObject(NSKeyValueCoding) kvc NSScriptClassDescription
+//        NSObject(NSKeyValueObserving)
+//        NSObject
+//        NSObject(NSScripting) NSObjectScripting
+//
+//        NSLocale 本地化         NSUserDefaults.h
+//
+//        NSNotification NSNotificationQueue 通知
     }
     return 0;
 }
